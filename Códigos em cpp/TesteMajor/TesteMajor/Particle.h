fa5d3642693cb0ddd7be12ee0dd50e54afc0341e
Nicolas <nico14142@gmail.com>
2019-06-18 13:58:05 -0300

    Upando teste no visual studio da main.cpp

---
+++ Códigos em cpp/TesteMajor/TesteMajor/Particle.h
@@ -0,0 +1,28 @@
+#if !defined(unix) && !defined(__unix__) && !defined(__unix)
+#pragma once
+#endif
+#ifndef __Particle
+#define __Particle
+
+
+class Particle
+{
+public:
+	double position[2];
+	double pbest_position[2];
+	double pbest_value;
+	double velocity[2];
+	double range = 5;
+	int id;
+
+	void set_position(double, double);
+	void str();
+	void move();
+	double random();
+	double velocity_mod();
+	void limit_velocity(double speed_limit);
+
+	//Particle & operator=(const Particle & p);
+	//Particle & operator+(const Particle & p) const;
+};
+#endif
